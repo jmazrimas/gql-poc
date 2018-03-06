cbff75e809b94a2d13758626841fb667f36947ce
Joe Mazrimas <joe.mazrimas@gmail.com>
2018-03-06 16:55:27 -0600

    implemented factory > datasource relationship

--- graph_ql/schema.py
+++ graph_ql/schema.py
@@ -2,35 +2,35 @@ import graphene
 from django.db import connection
 
 
-class Machine(graphene.ObjectType):
-    id = graphene.ID()
-    key = graphene.String()
-    factory_id = graphene.Int()
+# class Machine(graphene.ObjectType):
+#     id = graphene.ID()
+#     key = graphene.String()
+#     factory_id = graphene.Int()
 
 
 class Datasource(graphene.ObjectType):
     id = graphene.ID()
     name = graphene.String()
-    machine = graphene.Field(Machine,)
+    # machine = graphene.Field(Machine,)
     # factory = graphene.Field(Factory, )
 
-    def resolve_machine(self, info):
-        cursor = connection.cursor()
-        query = "select m.id, m.key, m.factory_id " \
-                "from machines m " \
-                "join datasources d on d.machine_id = m.key " \
-                "where d.id={}".format(self.id)
-        cursor.execute(query, (), )
-
-        results = cursor.fetchall()
-        if len(results) > 0:
-            return Machine(
-                id=results[0][0],
-                key=results[0][1],
-                factory_id=results[0][2],
-            )
-
-        return None
+    # def resolve_machine(self, info):
+    #     cursor = connection.cursor()
+    #     query = "select m.id, m.key, m.factory_id " \
+    #             "from machines m " \
+    #             "join datasources d on d.machine_id = m.key " \
+    #             "where d.id={}".format(self.id)
+    #     cursor.execute(query, (), )
+    #
+    #     results = cursor.fetchall()
+    #     if len(results) > 0:
+    #         return Machine(
+    #             id=results[0][0],
+    #             key=results[0][1],
+    #             factory_id=results[0][2],
+    #         )
+    #
+    #     return None
 
     # def resolve_factory(self, info):
     #     cursor = connection.cursor()
@@ -57,7 +57,6 @@ class Factory(graphene.ObjectType):
     datasources = graphene.List(Datasource)
 
     def resolve_datasources(self, info):
-        print('RESOLVE MACHINES')
         cursor = connection.cursor()
         query = "select d.id, d.name " \
                 "from machines m " \
@@ -69,7 +68,7 @@ class Factory(graphene.ObjectType):
         datasources = []
         for datasource in results:
             datasources.append(
-                Machine(
+                Datasource(
                     id=datasource[0],
                     name=datasource[1],
                 )
@@ -80,9 +79,9 @@ class Factory(graphene.ObjectType):
 class Query(graphene.ObjectType):
     datasources = graphene.List(Datasource)
     factories = graphene.List(Factory)
-    machine = graphene.Field(
-        Machine,
-        id=graphene.Int(),)
+    # machine = graphene.Field(
+    #     Machine,
+    #     id=graphene.Int(),)
 
     def resolve_datasources(self, info):
         cursor = connection.cursor()
@@ -117,25 +116,25 @@ class Query(graphene.ObjectType):
         return factories
 
 
-    def resolve_machine(self, info, **kwargs):
-        id = kwargs.get('id')
-
-        if id is not None:
-            cursor = connection.cursor()
-            query = "select id, key, factory_id from machines where id={}".format(id)
-            cursor.execute(query, (), )
-
-            results = cursor.fetchall()
-            machines = []
-            for machine in results:
-                machines.append(
-                    Machine(
-                        id=machine[0],
-                        key=machine[1],
-                        factory_id=machine[2],
-                    )
-                )
-
-            return machines[0]
-
-        return None
+    # def resolve_machine(self, info, **kwargs):
+    #     id = kwargs.get('id')
+    #
+    #     if id is not None:
+    #         cursor = connection.cursor()
+    #         query = "select id, key, factory_id from machines where id={}".format(id)
+    #         cursor.execute(query, (), )
+    #
+    #         results = cursor.fetchall()
+    #         machines = []
+    #         for machine in results:
+    #             machines.append(
+    #                 Machine(
+    #                     id=machine[0],
+    #                     key=machine[1],
+    #                     factory_id=machine[2],
+    #                 )
+    #             )
+    #
+    #         return machines[0]
+    #
+    #     return None
