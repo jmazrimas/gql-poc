f9bd13e06d630bc3ee10bf05e2c4de88e3331b56
Joe Mazrimas <joe.mazrimas@gmail.com>
2018-03-06 16:49:44 -0600

    refactoring relationships for top-down approach

--- graph_ql/schema.py
+++ graph_ql/schema.py
@@ -8,15 +8,11 @@ class Machine(graphene.ObjectType):
     factory_id = graphene.Int()
 
 
-class Factory(graphene.ObjectType):
-    id = graphene.ID()
-    name = graphene.String()
-
-
 class Datasource(graphene.ObjectType):
     id = graphene.ID()
     name = graphene.String()
     machine = graphene.Field(Machine,)
+    # factory = graphene.Field(Factory, )
 
     def resolve_machine(self, info):
         cursor = connection.cursor()
@@ -28,7 +24,6 @@ class Datasource(graphene.ObjectType):
 
         results = cursor.fetchall()
         if len(results) > 0:
-            print('RESULTS!!!!')
             return Machine(
                 id=results[0][0],
                 key=results[0][1],
@@ -37,9 +32,54 @@ class Datasource(graphene.ObjectType):
 
         return None
 
+    # def resolve_factory(self, info):
+    #     cursor = connection.cursor()
+    #     query = "select f.id, f.name " \
+    #             "from machines m " \
+    #             "join datasources d on d.machine_id = m.key " \
+    #             "join factory f on m.factory_id = f.id " \
+    #             "where d.id={}".format(self.id)
+    #     cursor.execute(query, (), )
+    #
+    #     results = cursor.fetchall()
+    #     if len(results) > 0:
+    #         return Factory(
+    #             id=results[0][0],
+    #             name=results[0][1],
+    #         )
+    #
+    #     return None
+
+
+class Factory(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    datasources = graphene.List(Datasource)
+
+    def resolve_datasources(self, info):
+        print('RESOLVE MACHINES')
+        cursor = connection.cursor()
+        query = "select d.id, d.name " \
+                "from machines m " \
+                "join datasources d on d.machine_id = m.key " \
+                "where m.factory_id={}".format(self.id)
+        cursor.execute(query, (), )
+
+        results = cursor.fetchall()
+        datasources = []
+        for datasource in results:
+            datasources.append(
+                Machine(
+                    id=datasource[0],
+                    name=datasource[1],
+                )
+            )
+        return datasources
+
 
 class Query(graphene.ObjectType):
     datasources = graphene.List(Datasource)
+    factories = graphene.List(Factory)
     machine = graphene.Field(
         Machine,
         id=graphene.Int(),)
@@ -59,6 +99,23 @@ class Query(graphene.ObjectType):
                 )
             )
         return datasources
+
+    def resolve_factories(self, info):
+        cursor = connection.cursor()
+        query = "select id, name from factory"
+        cursor.execute(query, (), )
+
+        results = cursor.fetchall()
+        factories = []
+        for factory in results:
+            factories.append(
+                Datasource(
+                    id=factory[0],
+                    name=factory[1],
+                )
+            )
+        return factories
+
 
     def resolve_machine(self, info, **kwargs):
         id = kwargs.get('id')
